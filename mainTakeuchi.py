8eabfd69afc334369358310b3c2aa7d6fcfe7418
Joshua Newington <50501647+09jnewington@users.noreply.github.com>
2023-11-16 00:13:42 +0000

    Update mainTakeuchi.py

--- mainTakeuchi.py
+++ mainTakeuchi.py
@@ -3,6 +3,8 @@ import random
 from rdkit import Chem
 from rdkit.Chem import Draw
 
+#DTX dummy atoms are [6, 9, 18, 23, 32]
+
 #List of common MedChem R groups from the Takeuchi Paper
 Takeuchi_list = r'smiles.txt.txt'
 
@@ -45,25 +47,49 @@ def mark_and_replace_atoms(smiles, important_atom_indices, num_variants=5):
 
     return modified_mols, None
 
-def reduce_structure(mol, important_indices=[]):
+def reduce_structure(mol, important_indices=None):
+    if important_indices is None:
+        important_indices = []
+
+    # Calculate connecting atoms
     connecting_atoms = set()
     for idx in important_indices:
         for jdx in important_indices:
             if idx != jdx:
                 path = Chem.rdmolops.GetShortestPath(mol, idx, jdx)
                 connecting_atoms.update(path)
-    keep_atoms = set(important_indices).union(connecting_atoms)
+
+    # Combined set of important and connecting atoms
+    combined_important_atoms = set(important_indices).union(connecting_atoms)
+
+    # Get ring info and determine which rings to keep
+    ring_info = mol.GetRingInfo()
+    rings_to_keep = set()
+    
+    for ring in ring_info.AtomRings():
+        if any(atom_idx in ring for atom_idx in combined_important_atoms):
+            rings_to_keep.update(ring)
+
+
+    # Determine atoms to keep
+    keep_atoms = combined_important_atoms.union(rings_to_keep)
+
+    # Remove unneeded atoms
     remove_atoms = [atom.GetIdx() for atom in mol.GetAtoms() if atom.GetIdx() not in keep_atoms]
     for idx in sorted(remove_atoms, reverse=True):
         mol = Chem.RWMol(mol)
         mol.RemoveAtom(idx)
+
     return mol
 
 def run():
     # Prompt the user for inputs
     smiles = input("Enter SMILES: ")
-    important_indices_str = input("Enter important atom indices (comma separated): ")
+    mol_count = Chem.MolFromSmiles(smiles)
+    num_atoms = mol_count.GetNumAtoms()
+    important_indices_str = input(f"The number of indices in your molecule is {num_atoms}, enter important atom indices (comma separated): ")
     num_variants_str = input("Number of modified variants: ")
+    
 
     num_variants = 5 if not num_variants_str else int(num_variants_str)
     important_indices = list(map(int, important_indices_str.split(',')))
@@ -74,7 +100,6 @@ def run():
         print(f"Error: {error}")
         sys.exit(1)
 
-    # Generate the image with original and modified molecules
     mols_to_draw = [Chem.MolFromSmiles(smiles)] + modified_molecules
     #img = Draw.MolsToGridImage(mols_to_draw, molsPerRow=4, subImgSize=(200,200), legends=["Original"] + ["Modified"] * num_variants)
     with Chem.SDWriter('sdf_output.sdf') as writer:
@@ -88,3 +113,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
